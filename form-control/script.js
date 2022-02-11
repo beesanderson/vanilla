e4c65aa09a72984fbf3fd7fa11758dc5bb84c2cc
beesanderson <87204074+beesanderson@users.noreply.github.com>
2022-02-11 14:08:02 -0600

    if statements

--- form-control/script.js
+++ form-control/script.js
@@ -7,9 +7,70 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 
+// ----------- Functions --------------
+
+// Show Input Error Message
+function showError(input, message) {
+    // .parentElement will get the form control div
+    const formControl = input.parentElement;
+    // change class to error color and display the small text below
+    formControl.className = 'form-control error';
+    // create variable for small text
+    const small = formControl.querySelector('small');
+    // display error message from submit event listener as small variable 
+    small.innerText = message;
+}
+
+
+// Show Success Outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    // change form input outline when successful
+    formControl.className = 'form-control success'
+}
+
+// Check Email Validity
+function isValidEmail(email) {
+    // With regular expression
+    // Just search "js email regex" for validation
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return re.test(String(email).toLowerCase());
+}
+
+
+
+
+
+// ---------------- Event Listeners ----------------------
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
+    if(username.value === '') {
+        showError(username, 'Username is Required')
+    } else {
+        showSuccess(username)
+    }
 
-    console.log(username.value)
+    if(email.value === '') {
+        showError(email, 'Email is Required')
+    } else if(!isValidEmail(email.value)) {
+        showError(email, 'Email is not Valid')
+    } else {
+        
+        showSuccess(email)
+    }
+
+    if(password.value === '') {
+        showError(password, 'Password is Required')
+    } else {  
+        showSuccess(password)
+    } 
+
+    if(password2.value === '') {
+        showError(password2, 'Reenter Password is Required')
+    } else {
+        showSuccess(password2)
+    }
+
+
 })
